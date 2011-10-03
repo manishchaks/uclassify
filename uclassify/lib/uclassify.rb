05da7be4e1927b2c10c37a00f63e96002f226ed5
Manish Chakravarty <manishchaks@gmail.com>
2011-10-03 16:21:00 -0600

    Refactoring + adding error handling + test for error handling

--- uclassify/lib/uclassify.rb
+++ uclassify/lib/uclassify.rb
@@ -3,12 +3,19 @@ require 'nokogiri'
 require File.join(File.dirname(__FILE__), 'uclassify_write_call.rb')
 require File.join(File.dirname(__FILE__), 'uclassify_create_id.rb')
 class UClassify
+  attr_accessor :write_api_key
+  attr_accessor :read_api_key
+  
   def initialize
-    puts "UClassify initialized"
     @write_calls = Array.new
   end
-  
-  def create_classifier(write_api_key,classifier_name,create_id)
+   
+  def create_classifier(classifier_name,create_id)
+    if (!@write_api_key) 
+      raise "No Write API Key for UClassify. Please specify one with 'uclassify.write_api_key=YOUR_KEY'"
+    end
+    
+    @write_api_key = write_api_key
     write_call = UClassifyWriteCall.new(write_api_key,classifier_name)
     id = UClassifyCreateID.new(create_id)
     write_call.add_create_id(id)
--- uclassify/spec/uclassify_spec.rb
+++ uclassify/spec/uclassify_spec.rb
@@ -5,7 +5,8 @@ require 'lib/uclassify'
 describe "UClassify base" do
   it "Should be able to a create a classifier" do
     uclassify = UClassify.new
-    uclassify.create_classifier('DUMMY_API_KEY','DUMMY_CLASSIFIER_NAME','DUMMY_CLASSIFIER_ID')
+    uclassify.write_api_key = 'DUMMY_API_KEY'
+    uclassify.create_classifier('DUMMY_CLASSIFIER_NAME','DUMMY_CLASSIFIER_ID')
     
     
     expected_string ="<?xml version=\"1.0\" encoding=\"utf-8\"?>
@@ -13,4 +14,15 @@ describe "UClassify base" do
     
     xml_cmp(uclassify.generate_request_string,expected_string).should == true
   end
+  
+  it "should not allow classifier creation and raise an exception when write api key has not been specified" do
+    begin
+      uclassify = UClassify.new
+      uclassify.create_classifier('DUMMY_CLASSIFIER_NAME','DUMMY_CLASSIFIER_ID')
+    rescue Exception => e
+      e.to_s.should == "No Write API Key for UClassify. Please specify one with 'uclassify.write_api_key=YOUR_KEY'"
+    end
+
+  end
+  
 end
